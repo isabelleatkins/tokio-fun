1608b3b4d910847480f8e4c88659e0418f2fa2bb
Isabelle Atkins <isabelleatkins@hotmail.co.uk>
2024-05-30 14:00:27 +0100

    cargo new

---
+++ tokio-fun/target/debug/deps/tokio_fun-1fca7b9e13c6a6f9.d
@@ -0,0 +1,5 @@
+/home/isabelleatkins/tokio-fun/tokio-fun/target/debug/deps/tokio_fun-1fca7b9e13c6a6f9.rmeta: src/main.rs
+
+/home/isabelleatkins/tokio-fun/tokio-fun/target/debug/deps/tokio_fun-1fca7b9e13c6a6f9.d: src/main.rs
+
+src/main.rs:
---
+++ tokio-fun/target/debug/deps/tokio_fun-e56aae60bb567307.d
@@ -0,0 +1,5 @@
+/home/isabelleatkins/tokio-fun/tokio-fun/target/debug/deps/tokio_fun-e56aae60bb567307.rmeta: src/main.rs
+
+/home/isabelleatkins/tokio-fun/tokio-fun/target/debug/deps/tokio_fun-e56aae60bb567307.d: src/main.rs
+
+src/main.rs:
